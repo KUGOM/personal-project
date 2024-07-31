c881778c002a2530d02015eac1464a19025f3cf9
KUGOM <yaeji0728@naver.com>
2024-07-31 10:58:16 +0900

    project middle

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,73 @@
+//예외처리 실패
+//inquiry가 값1개밖에 못받아옴 자동적으로 remove도 확인불가
 package calculator;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BadInputException {
+        //int[] results = new int[10];
+        LinkedList<Double> results = new LinkedList<>();//값을 무한히 저장하기 위해 LinkedList사용
+        double result = 0;
+        //int index = 0; //연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언
+        Scanner sc = new Scanner(System.in);
+        boolean re = true;
 
+        while (re) {
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int su1 = sc.nextInt();
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int su2 = sc.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+
+            Calculator calculator = new Calculator();
+            result = calculator.calculate(su1, su2, operator);
+
+            System.out.println("결과: " + result);
+
+            results.add(result);
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String remove = sc.next();
+            if (remove.equals("remove")) {/* remove을 입력 받으면 반복 종료 */
+                calculator.removeResults();
+                //results.removeFirst();//첫번쨰 결과 삭제
+            }
+
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            String inquiry = sc.next();
+            if (inquiry.equals("inquiry")) {
+                calculator.searchResults();
+                /*for (Double element : results) {
+                    System.out.print(element + " ");
+
+                }*/
+                System.out.println(" ");
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.next();
+            if (exit.equals("exit")) {/* exit을 입력 받으면 반복 종료 */
+                re = false;
+            }
+
+            /*if(index >=9) { //index값이 9를 넘어가면
+                for (int i = 0; i < results.length - 1; i++) { //인덱스 0부터 8번방까지
+                    results[index] = result;//다음 인데스의 값을 현재 인덱스 자리에 넣는다
+                }
+                results[index] = result; //인덱스 9에 결과값을 저장
+            }
+            else{//인덱스가 9 미만일때
+                results[index++] = result;
+            }*/
+        }
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,66 @@
 package calculator;
 
-public class Calculator {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Calculator {
+    private Queue<Double> results;//값을 무한히 저장하기 위해 LinkedList사용
+    static double radius;
+    final static double PI = 3.14159265358979323846;
+
+    public Calculator(){
+        this.results = new LinkedList<>();
+    }
+
+    public double calculateCircleArea(double radius){
+        this.radius = radius;
+        return PI * radius * radius;
+
+    }
+
+    public double calculate(int su1, int su2, char operator) throws BadInputException {
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = su1 + su2;
+                break;
+            case '-':
+                result = su1 - su2;
+                break;
+            case '*':
+                result = su1 * su2;
+                break;
+            case '/':
+                if (su2 != 0) {
+                    result = su1 / su2;
+                } else {
+                    throw new BadInputException("0이 아닌 정수");
+                }
+                break;
+        }
+        results.add(result);
+        return result;
+    }
+
+    public Queue<Double> getResults() {
+        return results;
+    }
+
+    public void setResults(double result) {
+        this.results.add(result);
+    }
+
+    public void removeResults(){
+        System.out.println("가장 먼저 저장된 "+ this.results.peek() +" 를 삭제합니다.");
+        //Queue.peek() - 삭제와 동시에 반환
+    }
+
+    public void searchResults() {
+        System.out.println("저장된 연산결과를 불러옵니다.");
+        Queue<Double> results = this.getResults();
+        for (double num : results) {
+            System.out.println(num);
+        }
+    }
 }
